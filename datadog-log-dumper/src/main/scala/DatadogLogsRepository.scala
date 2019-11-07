91d3d1ec132899d13971e439fe86291613e9a53a
Jason Martens <jmartens@zendesk.com>
2019-11-06 17:04:51 -0800

    Handle empty messages.

--- datadog-log-dumper/src/main/scala/DatadogLogsRepository.scala
+++ datadog-log-dumper/src/main/scala/DatadogLogsRepository.scala
@@ -13,15 +13,25 @@ trait DatadogLogsRepository extends LogsRepository {
 
   override val logsRepository: LogsRepository.Service[Any] = new LogsRepository.Service[Any] {
     override def submitLog(log: String): ZIO[Console, Throwable, SubmitResult] = {
-      val json = parse(log).transformField {
-        case JField("@timestamp", JDouble(num)) => ("@timestamp", JLong((num * 1000).toLong))
+      val request = try {
+        val json = parse(log).transformField {
+          case JField("@timestamp", JDouble(num)) => ("@timestamp", JLong((num * 1000).toLong))
+        }
+        val msg = compact(render(json))
+        basicRequest
+          .body(msg)
+          .contentType("application/json")
+          .post(uri"https://http-intake.logs.datadoghq.com/v1/input/${apiKey}?ddtags=env:development")
+      } catch {
+        case _: RuntimeException =>
+          basicRequest
+            .body(log)
+            .post(uri"https://http-intake.logs.datadoghq.com/v1/input/${apiKey}?ddtags=env:development")
+        case _: ParserUtil.ParseException =>
+          basicRequest
+            .body(log)
+            .post(uri"https://http-intake.logs.datadoghq.com/v1/input/${apiKey}?ddtags=env:development")
       }
-      val jsonString = compact(render(json))
-
-      val request = basicRequest
-        .body(jsonString)
-        .contentType("application/json")
-        .post(uri"https://http-intake.logs.datadoghq.com/v1/input/${apiKey}?ddtags=env:development")
       request.send().map {
         case response if response.code == StatusCode.Ok =>
           SubmitSuccess
